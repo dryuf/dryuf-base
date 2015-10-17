58cec17c202abf245b6fe245cc81badaabba23ae
kvr <kvr@centrum.cz>
2015-10-17 19:54:14 +0200

    asynchronous benchmarks

--- zbynek-lwfuture-pof/concurrent-lwfuture-pof-benchmark/src/main/java/cz/znj/kvr/sw/pof/concurrent/lwfuture/concurrent/benchmark/BenchmarkSupport.java
+++ zbynek-lwfuture-pof/concurrent-lwfuture-pof-benchmark/src/main/java/cz/znj/kvr/sw/pof/concurrent/lwfuture/concurrent/benchmark/BenchmarkSupport.java
@@ -18,4 +18,60 @@ public class BenchmarkSupport
 {
 	public static final int         WARMUP_ITERATIONS = 5;
 	public static final int         COUNT = 100000;
+
+	public static FutureTask<Integer>[] populateJdkFutureArray(int count)
+	{
+		Callable<Integer> func = () -> { return 0; };
+		FutureTask<Integer>[] array = new FutureTask[count];
+		for (int i = 0; i < count; ++i) {
+			array[i] = new FutureTask<Integer>(func);
+		}
+		return array;
+	}
+
+	public static cz.znj.kvr.sw.pof.concurrent.lwfuture.concurrent.ListenableFutureTask<Integer>[] populateLwFutureArray(int count)
+	{
+		Callable<Integer> func = () -> { return 0; };
+		cz.znj.kvr.sw.pof.concurrent.lwfuture.concurrent.ListenableFutureTask[] array = new cz.znj.kvr.sw.pof.concurrent.lwfuture.concurrent.ListenableFutureTask[count];
+		for (int i = 0; i < count; ++i) {
+			array[i] = new cz.znj.kvr.sw.pof.concurrent.lwfuture.concurrent.ListenableFutureTask<Integer>(func);
+		}
+		return array;
+	}
+
+	public static org.springframework.util.concurrent.ListenableFutureTask<Integer>[] populateSpringFutureArray(int count)
+	{
+		Callable<Integer> func = () -> { return 0; };
+		org.springframework.util.concurrent.ListenableFutureTask[] array = new org.springframework.util.concurrent.ListenableFutureTask[count];
+		for (int i = 0; i < count; ++i) {
+			array[i] = new org.springframework.util.concurrent.ListenableFutureTask(func);
+		}
+		return array;
+	}
+
+	public static com.google.common.util.concurrent.ListenableFutureTask<Integer>[] populateGuavaFutureArray(int count)
+	{
+		Callable<Integer> func = () -> { return 0; };
+		com.google.common.util.concurrent.ListenableFutureTask[] array = new com.google.common.util.concurrent.ListenableFutureTask[count];
+		for (int i = 0; i < count; ++i) {
+			array[i] = com.google.common.util.concurrent.ListenableFutureTask.create(func);
+		}
+		return array;
+	}
+
+	public static void                     threadedRunFutures(RunnableFuture<Integer>[] array)
+	{
+		Thread t = new Thread(() -> {
+			for (RunnableFuture<Integer> v : array) {
+				v.run();
+			}
+		});
+		t.start();
+		try {
+			t.join();
+		}
+		catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
